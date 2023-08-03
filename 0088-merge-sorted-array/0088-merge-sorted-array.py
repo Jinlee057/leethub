b521c957ebf39cdcc788492db1159d6e2e301b2b
Jin <117559875+Jinlee057@users.noreply.github.com>
2023-08-03 14:13:02 +0900

    Time: 46 ms (84.59%), Space: 16.1 MB (98.65%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -4,9 +4,9 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         while n > 0:
-            if nums1[m-1] >= nums2[n-1] and m>0:
-                nums1[m+n-1] = nums1[m-1]
+            if nums1[m-1] >= nums2[n-1] and m>0: #각 리스트에서 마지막값끼리 비교
+                nums1[m+n-1] = nums1[m-1] #nums1의 전체 길이는 m+n, 가장 마지막에 둘 중 큰값
                 m -= 1
             else:
                 nums1[m+n-1] = nums2[n-1]
-                n -= 1
+                n -= 1                  #n이 0이되면 nums2 리스트내 모든 값 확인 완료
